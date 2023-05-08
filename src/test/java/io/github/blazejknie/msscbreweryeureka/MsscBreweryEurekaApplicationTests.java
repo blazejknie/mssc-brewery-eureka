60a01ee1420d1f78e165bfc69827afc44f915101
Blazej_Knie <bjke@gft.com>
2023-05-08 12:02:31 +0200

    Initial add

---
+++ src/test/java/io/github/blazejknie/msscbreweryeureka/MsscBreweryEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.blazejknie.msscbreweryeureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryEurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
